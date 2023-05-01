5e44ed68fc661750fd5048709b9c9c2a09068052
Voicu Gabriel <g_the_reaper@yahoo.com>
2023-05-01 17:59:12 +0200

    added prix ttc

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function render(array) {
   array.forEach((element) => {
     li =
       li +
-      `<li>Nom boisson: <input type="text" value="${element.boisson}"> Quantite: <input type="number" value="${element.quantite}"> Prix d'achat: <input type="number" value="${element.prixAchate}"> Prix de vente: <input type="number" value="${element.prixVente}"> Marge: ${element.marge}% </li>`;
+      `<li>Nom boisson: <input type="text" value="${element.boisson}"> Quantite: <input type="number" size=1 value="${element.quantite}"> Prix d'achat: <input type="number" value="${element.prixAchate}"> Prix de vente: <input type="number" value="${element.prixVente}"> Marge: <input type="number" value="${element.marge}">% Prix de vente TTC <input type="number" value="${element.prixVenteTtc}">  </li>`;
   });
   ulContainer.innerHTML = li;
+  
 }
 function addProduit(e) {
   e.preventDefault();
@@ -18,12 +19,18 @@ function addProduit(e) {
     data.get("quantite"),
     data.get("prixAchate"),
     data.get("prixVente"),
-    (data.get("prixVente") / data.get("prixAchate")) * 100
-    //prixVenteTtc: prixVente * tva,
+    (data.get("prixVente") / data.get("prixAchate")) * 100,
+    data.get("prixVente") * 1.2 ,
     //typeBoisson: type.value,
-    // degre function
+    //if (type boisson=alco) {
+    //  1.creation de button degree
+    //  2.button degree add to form
+    //   // degre function 
+    // }
+    
   );
   inventaire.push(produit);
+  console.log(inventaire);
   render(inventaire, "all");
 }
 
@@ -35,6 +42,7 @@ function Produit(
   prixAchate,
   prixVente,
   marge,
+  prixVenteTtc,
   type,
   degreAlcool
 ) {
@@ -43,6 +51,7 @@ function Produit(
   this.prixAchate = prixAchate;
   this.prixVente = prixVente;
   this.marge = marge;
+  this.prixVenteTtc = prixVenteTtc
   this.type = type;
   this.degreAlcool = degreAlcool;
 }
