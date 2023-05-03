d4aad593af694c2abf33784fe1bbe101c5277aa9
GabrielVoicu89 <g_the_reaper@yahoo.com>
2023-05-03 09:48:51 +0200

    commit bocal
    Co-authored-by: Malekbennasser <Malekbennasser@users.noreply.github.com>
    Co-authored-by: Léa <Lazarona@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
 let ulContainer = document.querySelector(".ulContainer");
 let form = document.querySelector(".form");
 let inventaire = [];
+var qrcode = new QRCode("qrcode");
 function render(array) {
   let li = "";
-  array.forEach((element) => {
+  array.forEach((element, index) => {
     if (element.type === "nonAlco") {
       li =
-      li +
-      `<li>Nom boisson: <input type="text" value="${element.boisson}"> Quantite: <input type="number" class="qnt" value="${element.quantite}"> Prix d'achat: <input type="number" value="${element.prixAchate}"> Prix de vente: <input type="number" value="${element.prixVente}"> Marge % : <input type="number" value="${element.marge}"> Prix de vente TTC <input type="number" value="${element.prixVenteTtc}">  </li>`;
+        li +
+        `<li>Nom boisson: <input class="inputBoisson" type="text" value="${element.boisson}"readonly> 
+         Quantite: <input class="inputQuantite" type="number" value="${element.quantite}"readonly> 
+         Prix d'achat: <input class="inputPrixAchate" type="number" value="${element.prixAchate}"readonly> 
+         Prix de vente: <input type="number" class="inputPrixVente" value="${element.prixVente}"readonly> 
+         Marge % : <input type="number" class="inputMarge" value="${element.marge}"readonly> 
+         Prix de vente TTC <input type="number" class="inputPrixVenteTtc" value="${element.prixVenteTtc}"readonly> 
+         <button type='button' class='btn-Mof'>EDIT</button>  </li>`;
     } else {
       li =
-      li +
-      `<li>Nom boisson: <input type="text" value="${element.boisson}"> Quantite: <input type="number" value="${element.quantite}"> Prix d'achat: <input type="number" value="${element.prixAchate}"> Prix de vente: <input type="number" value="${element.prixVente}"> Marge % : <input type="number" value="${element.marge}"> Prix de vente TTC <input type="number" value="${element.prixVenteTtc}"> Degree alcool: <input type="number" value="${element.degre}"> </li>`;
+        li +
+        `<li>Nom boisson: <input class="inputList" type="text" value="${element.boisson}"readonly> Quantite: <input class="inputList" type="number" value="${element.quantite}"readonly> Prix d'achat: <input type="number" class="inputList" value="${element.prixAchate}"readonly> Prix de vente: <input type="number" class="inputList" value="${element.prixVente}"readonly> Marge % : <input type="number" class="inputList" value="${element.marge}"readonly> Prix de vente TTC <input type="number" class="inputList" value="${element.prixVenteTtc}"readonly> Degree alcool: <input type="number" class="inputList" value="${element.degre}"readonly> <button type='button' class='btn-Mof'>EDIT</button> </li>`;
     }
-    
   });
   ulContainer.innerHTML = li;
-  
+  let allButton = document.querySelectorAll(".btn-Mof");
+  allButton.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      let inputBoisson = document.querySelectorAll(".inputBoisson");
+      let inputQuantite = document.querySelectorAll(".inputQuantite");
+      let inputPrixAchate = document.querySelectorAll(".inputPrixAchate");
+      let inputPrixVente = document.querySelectorAll(".inputPrixVente");
+      let inputMarge = document.querySelectorAll(".inputMarge");
+      let inputPrixVenteTtc = document.querySelectorAll(".inputPrixVenteTtc");
+
+      if (element.innerText.toLowerCase() == "edit") {
+        element.innerText = "SAVE";
+        inputBoisson[index].removeAttribute("readonly");
+        inputQuantite[index].removeAttribute("readonly");
+        inputPrixAchate[index].removeAttribute("readonly");
+        inputPrixVente[index].removeAttribute("readonly", "readonly");
+        inputMarge[index].removeAttribute("readonly");
+        inputPrixVenteTtc[index].removeAttribute("readonly");
+      } else {
+        element.innerText = "EDIT";
+        inputBoisson[index].setAttribute("readonly", "readonly");
+        inputQuantite[index].setAttribute("readonly", "readonly");
+        inputPrixAchate[index].setAttribute("readonly", "readonly");
+        inputPrixVente[index].setAttribute("readonly", "readonly");
+        inputMarge[index].setAttribute("readonly", "readonly");
+        inventaire[index].boisson = inputBoisson[index].value;
+        inventaire[index].quantite = inputQuantite[index].value;
+        inventaire[index].prixAchate = inputPrixAchate[index].value;
+        inventaire[index].prixVente = inputPrixVente[index].value;
+        inventaire[index].marge =
+          (inputPrixVente[index].value / inputPrixAchate[index].value) * 100;
+        render();
+        console.log(inventaire);
+      }
+    });
+  });
 }
 function addProduit(e) {
   e.preventDefault();
@@ -26,14 +67,14 @@ function addProduit(e) {
       data.get("boisson"),
       data.get("quantite"),
       data.get("prixAchate"),
-      data.get("prixVente"),                              
+      data.get("prixVente"),
       ((data.get("prixVente") / data.get("prixAchate")) * 100).toFixed(2), // .toFixed(2) make the number with only 2 decimals
-      data.get("prixVente") * 1.2, 
+      data.get("prixVente") * 1.2,
       data.get("type")
     );
     inventaire.push(produit);
-  console.log(inventaire);
-  render(inventaire, "all");
+    console.log(inventaire);
+    render(inventaire, "all");
   } else {
     let produit = new Produit(
       data.get("boisson"),
@@ -41,14 +82,14 @@ function addProduit(e) {
       data.get("prixAchate"),
       data.get("prixVente"),
       ((data.get("prixVente") / data.get("prixAchate")) * 100).toFixed(2),
-      data.get("prixVente") * 1.2 ,
+      data.get("prixVente") * 1.2,
       data.get("type"),
-      data.get("degre")  
+      data.get("degre")
     );
     inventaire.push(produit);
     console.log(inventaire);
     render(inventaire, "all");
-  } 
+  }
 }
 
 form.addEventListener("submit", addProduit);
@@ -62,14 +103,13 @@ function Produit(
   prixVenteTtc,
   type,
   degre
-)
-{
+) {
   this.boisson = boisson;
   this.quantite = quantite;
   this.prixAchate = prixAchate;
   this.prixVente = prixVente;
   this.marge = marge;
-  this.prixVenteTtc = prixVenteTtc
+  this.prixVenteTtc = prixVenteTtc;
   this.type = type;
   this.degre = degre;
 }
